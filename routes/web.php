68c5302033ff398ce391793b574e9394fb73290c
Anthony <anthony.bouillant@cpnv.ch>
2021-12-09 17:42:44 +0100

    Update web to mandatory authentication

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/', function () {
+        return view('welcome');
+    });
 });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
