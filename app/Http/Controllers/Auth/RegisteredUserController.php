45b6480464ae62a0a28b20d563ea317e1627b486
Anthony Bouillant <anthony.bouillant@cpnv.ch>
2021-12-16 16:36:28 +0100

    Fix : registerUserController check unique fullname

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -35,7 +35,7 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
-            'fullname' => ['required', 'string', 'max:255'],
+            'fullname' => ['required', 'string', 'max:255', 'unique:users'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
