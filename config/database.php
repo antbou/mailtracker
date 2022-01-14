ea578ce529f35a9f646020ed4c62b4aed4669753
Anthony Bouillant <anthony.bouillant@cpnv.ch>
2022-01-14 12:05:53 +0100

    Init mongodb

--- config/database.php
+++ config/database.php
@@ -91,6 +91,14 @@ return [
             'prefix_indexes' => true,
         ],
 
+        'mongodb' => [
+            'driver' => 'mongodb',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', 27017),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', ''),
+            'password' => env('DB_PASSWORD', '')
+        ],
     ],
 
     /*
